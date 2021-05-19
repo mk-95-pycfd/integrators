99ddbb65d8aeb5968131d8f148af96ff5fb27689
mk-95 <karammokbel@gmail.com>
2021-05-19 13:19:43 -0600

    added post-processing pressure RK4_taylor_vortex

--- RK4_taylor_vortex.py
+++ RK4_taylor_vortex.py
@@ -4,7 +4,7 @@ import time
 import statistics
 import singleton_classes as sc
 
-def RK4_taylor_vortex (steps = 3,return_stability=False,name='regular',guess=None,project=[1,1,1],alpha=0.99):
+def RK4_taylor_vortex (steps = 3,return_stability=False,name='regular',guess=None,project=[1,1,1],alpha=0.99,post_projection=False):
     # problem description
     probDescription = sc.ProbDescription()
     f = func(probDescription,'periodic')
@@ -199,11 +199,12 @@ def RK4_taylor_vortex (steps = 3,return_stability=False,name='regular',guess=Non
         unp1,vnp1,press,iter3= f.ImQ(uhnp1,vhnp1,Coef,pn)
         time_end = time.clock()
 
-        # post processing projection
-        unp1r = dt * f.urhs(unp1, vnp1)
-        vnp1r = dt * f.vrhs(unp1, vnp1)
+        if post_projection:
+            # post processing projection
+            uhnp1_star = u + dt * (f.urhs(unp1, vnp1))
+            vhnp1_star = v + dt * (f.vrhs(unp1, vnp1))
 
-        # _, _, press, _ = f.ImQ_post_processing(unp1r, vnp1r, Coef, pn)
+            _, _, post_press, _ = f.ImQ(uhnp1_star, vhnp1_star, Coef, pn)
 
         psol.append(press)
         cpu_time = time_end - time_start
